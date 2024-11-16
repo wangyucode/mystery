812b0924e9cf8eac56d6b44879fadfccdf09be7e
yu.wang <wangyu@wycode.cn>
2024-11-17 05:17:49 +0800

    remove role modal

--- src/components/ExtraRoleContent.jsx
+++ src/components/ExtraRoleContent.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ModalBody, Avatar, Button, ModalFooter, Card, CardBody } from "@nextui-org/react";
+import { Avatar, Button, Card, CardBody, Divider } from "@nextui-org/react";
 
 import socket from "../socket";
 
@@ -18,28 +18,25 @@ export default function ExtraRoleContent({ extra, room }) {
     }
 
     return (
-        <>
-            <ModalBody>
-                <p className="text-sm whitespace-pre-line">
-                    {extra.background}
-                </p>
-                <div className="flex gap-4">
-                    {extra.roles.map((r) => (
-                        <Card isPressable onClick={selectRole} data-name={r.name} key={r.name} className={`${r.name === role.name ? "ring-4" : ""}`}>
-                            <CardBody className="flex flex-col items-center gap-2">
-                                <Avatar src={`/avatars/${room.title}/${r.name}.png`} alt={r.name} size="lg" />
-                                <b>{r.name}</b>
-                            </CardBody>
-                        </Card>
-                    ))}
-                </div>
-                <p className="text-sm whitespace-pre-line">
-                    {role.desc}
-                </p>
-            </ModalBody>
-            <ModalFooter>
-                <Button color="primary" onClick={sendRole} isDisabled={!!self.role}>选择</Button>
-            </ModalFooter>
-        </>
+        <div className="flex flex-col gap-2 items-start">
+            <p className="text-sm whitespace-pre-line">
+                {extra.background}
+            </p>
+            <div className="flex gap-4">
+                {extra.roles.map((r) => (
+                    <Card isPressable onClick={selectRole} data-name={r.name} key={r.name} className={`${r.name === role.name ? "ring-4" : ""}`}>
+                        <CardBody className="flex flex-col items-center gap-2">
+                            <Avatar src={`/avatars/${room.title}/${r.name}.png`} alt={r.name} size="lg" />
+                            <b>{r.name}</b>
+                        </CardBody>
+                    </Card>
+                ))}
+            </div>
+            <p className="text-sm whitespace-pre-line">
+                {role.desc}
+            </p>
+            <Divider />
+            <Button color="primary" onClick={sendRole} isDisabled={!!self.role} className="self-end">选择</Button>
+        </div>
     );
 }
--- src/components/SystemMessage.jsx
+++ src/components/SystemMessage.jsx
@@ -1,14 +1,12 @@
-import { Card, CardBody, Avatar, Button } from "@nextui-org/react";
+import { Card, CardBody, Avatar } from "@nextui-org/react";
 import { useState, useEffect } from "react";
-import { ServerStackIcon, ArrowsPointingOutIcon } from "@heroicons/react/16/solid";
+import { ServerStackIcon } from "@heroicons/react/16/solid";
 
 import socket from "../socket";
-import ExtraModal from "./ExtraModal";
+import ExtraRoleContent from "./ExtraRoleContent";
 
 
 export default function SystemMessage({ message, room }) {
-
-    const [openExtra, setOpenExtra] = useState(false);
 
     useEffect(() => {
         socket.on("room:role:success", handleRoleSuccess);
@@ -17,43 +15,27 @@ export default function SystemMessage({ message, room }) {
         };
     }, []);
 
+
     function handleRoleSuccess() {
-        setOpenExtra(false);
+        setIsSelected(true);
     }
 
     return (
         <Card className="self-start">
-            <CardBody className="flex flex-row items-center">
+            <CardBody className="flex flex-row">
                 <Avatar
                     className="w-7 h-7 text-tiny mr-2"
                     icon={<ServerStackIcon className="size-4" />}
                     color="secondary"
                 />
-                <div className="flex flex-col">
+                <div className="flex flex-col flex-1">
                     <b className="text-xs">{message.from}</b>
                     <p className="text-sm">
                         <span className="text-sky-500">@{message.to}：</span>
                         {message.content}
+                        {message.extra?.roles && <ExtraRoleContent extra={message.extra} room={room} />}
                     </p>
                 </div>
-                {message.extra && (
-                    <Button
-                        className="ml-2"
-                        color="primary"
-                        size="sm"
-                        endContent={<ArrowsPointingOutIcon className="size-4" />}
-                        onClick={() => setOpenExtra(true)}
-                    >
-                        详情
-                        <ExtraModal
-                            open={openExtra}
-                            onClose={() => setOpenExtra(false)}
-                            extra={message.extra}
-                            title={message.content}
-                            room={room}
-                        />
-                    </Button>
-                )}
             </CardBody>
         </Card>
     );
