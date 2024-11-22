9553c203b99f992088846b5e9f29a1fb838ea9b0
yu.wang <wangyu@wycode.cn>
2024-11-22 11:59:58 +0800

    host mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect} from "react";
-import { Link } from "@nextui-org/react";
+import { useState, useEffect } from "react";
+import { Link, Chip } from "@nextui-org/react";
 import { Outlet } from "react-router-dom";
 import socket from "./socket";
 
 export default function App() {
 
-  const [count, setCount] = useState({ user: 0, ai: 500, room: 0 });
+  const [count, setCount] = useState({ user: 0, ai: 0, room: 0 });
 
   useEffect(() => {
     console.log("socket->", socket);
@@ -22,7 +22,11 @@ export default function App() {
     <div className="flex flex-col min-h-screen gap-4 items-center text-center">
       <header className="mt-4">
         <h1 className="text-2xl m-2">AI剧本杀</h1>
-        <p className="text-sm">在线人类：{count.user}，在线AI：{count.ai}，房间数：{count.room}</p>
+        <div className="flex gap-2 items-center">
+          <Chip size="sm" color="primary" variant="flat">在线人数：{count.user}</Chip>
+          <Chip size="sm" color="secondary" variant="flat">在线AI：{count.ai}</Chip>
+          <Chip size="sm" color="warning" variant="flat">房间数：{count.room}</Chip>
+        </div>
       </header>
       <Outlet />
       <footer className='border-t p-2 text-center w-full flex items-center justify-center text-sm gap-2'>
